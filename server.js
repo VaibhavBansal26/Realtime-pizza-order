675bdde0337683d81c6d827ab4cb22fc00234514
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2021-05-26 23:50:51 +0530

    Cart functionality completed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -9,11 +9,59 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 5000
 
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
+//Database Connection
 
+//CONFIG FILE
+const uri = 'mongodb+srv://V_Bansal:kritisanon14@cluster0.hljid.mongodb.net/pizza-app?retryWrites=true&w=majority'
+
+//MONGO DB CONNECTION
+mongoose.connect(uri,{
+    useNewUrlParser : true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology: true
+}).then(con => {
+    console.log("Connection is established")
+})
+
+const connection = mongoose.connection;
+connection.once('open',() => {
+    console.log("yeah connected... ");
+}).catch(err =>{
+    console.log('Connection failed...')
+})
+
+//session store
+// let mongoStore = new MongoDbStore({
+//     mongooseConnection:connection,
+//     collection:'sessions'
+// })
+
+//session
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    store:MongoDbStore.create({
+        client:connection.getClient(),
+        mongoUrl:uri
+    }),
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24}
+}))
+
+app.use(flash())
 //Assets
 app.use(express.static('public'))
-
-
+app.use(express.json())
+//Global middleware
+app.use((req,res,next) => {
+    res.locals.session = req.session
+    next()
+})
 
 //Set Template Engine
 app.use(expressLayout)
@@ -21,19 +69,7 @@ app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 //Routes
-app.get('/',(req,res) =>{
-    res.render('home')
-})
-
-app.get('/cart',(req,res) =>{
-    res.render('customer/cart')
-})
-app.get('/login',(req,res) =>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res) =>{
-    res.render('auth/register')
-})
+require('./routes/web')(app)
 
 app.listen(PORT,() =>{
     console.log(`Listening on port ${PORT}`)
